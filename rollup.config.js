480d18d5b308797d764612a705c721fb60459363
Andy Pan <andypan@ntuosc.org>
2023-07-12 02:49:07 +0800

    Upgrade deps; fix demo

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,26 @@
 import node from "@rollup/plugin-node-resolve";
-import {terser} from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import meta from "./package.json" assert {type: "json"};
 
 const copyright = `// @observablehq/inspector v${meta.version} Copyright ${(new Date).getFullYear()} Observable, Inc.`;
+
+const output = process.env.CI ? {
+  format: 'esm',
+  file: 'dist/inspector.esm.js',
+} : {
+  format: "umd",
+  extend: true,
+  name: "observablehq",
+  file: "dist/inspector.js"
+}
 
 export default [
   {
     input: "src/index.js",
     plugins: [
       node(),
-      terser({output: {preamble: copyright}})
+      process.env.CI ? null : terser({output: {preamble: copyright}})
     ],
-    output: {
-      format: "umd",
-      extend: true,
-      name: "observablehq",
-      file: "dist/inspector.js"
-    }
+    output,
   }
 ];
